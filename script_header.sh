3c2a172f3962f2a86c72819fcd40ad521739ca6d
joestandring <git@joestandring.com>
2019-05-19 10:29:23 +0000

    Make the script actually POSIX compliant.

--- script_header.sh
+++ script_header.sh
@@ -4,52 +4,42 @@
 # Joe Standring <git@joestandring.com>
 # GNU GPLv3
 
-# Store current directory
-DIR=${BASH_SOURCE%/}
-if [[ ! -d "$DIR" ]]; then
-    DIR="$PWD" 
-fi  
-
 # User inputs file location
-read -e -p "File location (/path/to/script.sh): " FILE
+read -r -p "File location (/path/to/script.sh): " FILE
 
 # Removes spaces and converts captials to lower case
 FILE=$(printf "$FILE" | tr -d "[:space:]" | tr "[:upper:]" "[:lower:]")
 
-# Parameter expansion
-FILE="${FILE/#\~/$HOME}"
-
 # If the file already exists, inform the user and exit
-if [ -e $FILE ]; then
+if [ -e "$FILE" ]; then
     printf "This file already exists\n"
     exit
 fi
 
 # Record other information
-read -p "Description: " DESC
-read -p "Full name: " NAME
-read -p "Email address: " EMAIL
-read -p "License used: " LICENSE
+read -r -p "Description: " DESC
+read -r -p "Full name: " NAME
+read -r -p "Email address: " EMAIL
+read -r -p "License used: " LICENSE
 
 # Ask if user wants to include a section listing dependencies
-read -p "Do you want to include a dependencies section? (y/n): " DEPLIST
+read -r -p "Do you want to include a dependencies section? (y/n): " DEPLIST
 
 if [ "$DEPLIST" = "y" ]; then
-    read -p "List dependencies seperated by a space: " DEPS
-    DEPS=("\n\n# Dependencies: $(printf "$DEPS" | sed 's/ \{1,\}/, /g')")
+    read -r -p "List dependencies seperated by a space: " DEPS
 fi
 
 # Print script header to file
-printf "#!/bin/sh\n\n# $DESC\n# $NAME <$EMAIL>\n# $LICENSE$DEPS\n\n\n" > $FILE
+printf "#!/bin/sh\n\n# $DESC\n# $NAME <$EMAIL>\n# $LICENSE\n\n# Dependencies: $DEPS\n\n\n" > "$FILE"
 
 # Make file executable
-chmod +x $FILE
+chmod +x "$FILE"
 
 # Ask if the user wants to open the file now
-read -p "Do you want to open the file now? (y/n): " OPEN
-OPEN=$(echo $OPEN | tr "[:upper:]" "[:lower:]")
+read -r -p "Do you want to open the file now? (y/n): " OPEN
+OPEN=$(echo "$OPEN" | tr "[:upper:]" "[:lower:]")
 
 if [ "$OPEN" = "y" ]; then
-    vim +9 $FILE
+    vim +9 "$FILE"
 fi
 
